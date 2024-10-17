e19a8f3fe3969dde539ad0adc2524c693e92af8d
Arpan <arpannema1234@gmail.com>
2024-10-17 18:59:26 +0530

    changed wholesaler and retailer to user

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const authRoutes = require('./authRoutes');
-const adminRoutes = require('./adminRoutes');
-const retailerRoutes = require('./retailerRoutes');
-const wholesalerRoutes = require('./wholesalerRoutes');
-const deliveryPartnerRoutes = require('./deliveryPartnerRoutes');
-
+const express = require("express");
+const authRoutes = require("./authRoutes");
+const adminRoutes = require("./adminRoutes");
+const userAuthRoutes = require("./userAuthRoutes");
 const router = express.Router();
 
-router.use('/auth', authRoutes);
-router.use('/admin', adminRoutes);
-router.use('/retailer', retailerRoutes);
-router.use('/wholesaler', wholesalerRoutes);
-router.use('/delivery-partner', deliveryPartnerRoutes);
+// For sending and verifying OTP
+router.use("/auth", authRoutes);
+
+// For admin signup and login
+router.use("/admin", adminRoutes);
+
+// For user signup and login
+router.use("/user", userAuthRoutes);
 
 module.exports = router;
